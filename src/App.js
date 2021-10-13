5eedf8d50725c9cee73cbcc393b1c6610c1d9a72
eledoro <lena@booggee.com>
2021-10-13 11:53:43 +0100

    Added navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import ArtistCard from "./components/ArtistCard";
 import AlbumCard from "./components/AlbumCard";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
+import {
+  BrowserRouter as Router, Switch,
+  Route, Link, NavLink, useRouteMatch, useParams
+} from 'react-router-dom';
+import BackButton from './components/BackButton';
+import Navbar from "./components/Navbar";
+
 
 function App() {
+
   return (
-    <main>
-      <h1>Favorite Artist</h1>
-      <ArtistCard />
-      <AlbumCard />
-    </main>
+    <Router>
+      <main>
+        <Navbar />
+        <Switch>
+          <Route exact path="/"> Home page. Choose something</Route>
+
+          <Route exact path="/about">
+            <ArtistCard />
+          </Route>
+
+          <Route exact path="/albums">
+            <AlbumCard />
+          </Route>
+        </Switch>
+
+      </main>
+    </Router>
   );
 }
 
